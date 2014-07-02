7df68acf000851f4a748d730a5a1132f507b17ec
tomgp <tom.g.pearson@gmail.com>
2014-07-02 17:38:39 +0100

    adding line chart and category axis scaffold

---
+++ js/chart/ft-linechart.js
@@ -0,0 +1,18 @@
+'use strict'
+
+if(!ft){
+	var ft = {};
+}
+
+if(!ft.charts){
+	ft.charts = {};
+}
+
+ft.charts.line = function(){
+
+	var lines = [];
+
+	function chartdraw(g){}
+
+	return chartdraw;
+}
