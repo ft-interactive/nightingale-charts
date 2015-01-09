8ca02bc62fc5764c4b26fe5f149b5812121aaa0c
tomgp <tom.g.pearson@gmail.com>
2015-01-09 11:51:27 +0000

    actually, that doesn't quite work, trying something else...

--- src/axis/date.js
+++ src/axis/date.js
@@ -149,29 +149,27 @@ function dateAxis() {
 		}
 
 //check for and remove overlaping labels
-		var bounds = [];
-		g.selectAll('.tick').each(function(d,i){
-			//check whether it overlaps any of the existing bounds
-			var rect = this.getBoundingClientRect();
-			var include = true;
-			var current = d3.select(this);
-			bounds.forEach(function(b,i){
-				console.log(d);
-				if(intersection(b,rect)){
-					//if it does, remove it
-					console.log('remove', current);
-					current.remove();
-					include = false;
-				}
-			});
-
-			if(include){
-				bounds.push(rect);
-				console.log('b' + bounds.length);
-			}
-
-
-		});
+		// var bounds = [];
+		// g.selectAll('.tick').each(function(d,i){
+		// 	//check whether it overlaps any of the existing bounds
+		// 	var rect = this.getBoundingClientRect();
+		// 	var include = true;
+		// 	var current = d3.select(this);
+		// 	bounds.forEach(function(b,i){
+		// 		console.log(d);
+		// 		if(intersection(b,rect)){
+		// 			//if it does, remove it
+		// 			console.log('remove', current);
+		// 			current.remove();
+		// 			include = false;
+		// 		}
+		// 	});
+		//
+		// 	if(include){
+		// 		bounds.push(rect);
+		// 		console.log('b' + bounds.length);
+		// 	}
+		// });
 	}
 
 	function intersection(a, b){
