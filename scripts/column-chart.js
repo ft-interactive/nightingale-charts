ed3465d3404356a05e2951484fee87b3e0043514
circleci <peter.mouland@ft.com>
2015-04-30 21:15:33 +0100

    v0.0.8

--- scripts/column-chart.js
+++ scripts/column-chart.js
@@ -664,39 +664,40 @@ var DataModel = require('../util/data.model.js');
 var metadata = require('../util/metadata.js');
 var Dressing = require('../util/dressing.js');
 
-function plotSeries(plotSVG, model, axes, series) {
+function plotSeries(plotSVG, model, axes, series, i){
+	var data = formatData(model, series);
+	var s = plotSVG.append('g')
+		.attr('class', 'series');
 
-    var data = formatData(model, series);
+//todo:  sort out scale so you dont have to do the maths for width & x-position
+//	http://bl.ocks.org/mbostock/3887051
+//console.log(data)
+//	var x1= d3.scale.ordinal()
+//	var bars = d3.keys(data).filter(function(key) { return key !== "State"; });
+//	console.log(bars)
+//	x1.domain(bars).rangeRoundBands([0, axes.timeScale.rangeBand()]);
 
-    plotSVG.selectAll("rect")
+    s.selectAll('rect')
         .data(data)
         .enter()
-        .append("rect")
-        .attr("class", function (d) {
-            return "column column--" + series.className + (d.value < 0 ? " negative" : " positive");
-        })
-        .attr("data-value", function (d) {
-            return d.value;
-        })
-        .attr("x", function (d) {
-            return axes.timeScale(d.key);
-        })
-        .attr("y", function (d) {
-            return axes.valueScale(Math.max(0, d.value));
-        })
-        .attr("height", function (d) {
-            return Math.abs(axes.valueScale(d.value) - axes.valueScale(0));
-        })
-        .attr("width", axes.timeScale.rangeBand());
+        .append('rect')
+        .attr('class', function (d){return 'column column--'  + series.className + (d.value < 0 ? ' negative' : ' positive');})
+        .attr('data-value', function (d){	return d.value;})
+		.attr('x', function (d){return axes.timeScale(d.key) + ((axes.timeScale.rangeBand() / model.y.series.length) * i);}) //adjust the x position based on the series number
+		//.attr("x", function(d) { return x1(d.key); })
+		.attr('y', function (d){return axes.valueScale(Math.max(0, d.value));})
+        .attr('height', function (d){return 	Math.abs(axes.valueScale(d.value) - axes.valueScale(0));})
+		//.attr("width", x1.rangeBand())
+        .attr('width', axes.timeScale.rangeBand() / model.y.series.length); //width is divided by series length
 }
 
 function formatData(model, series) {
     //null values in the data are interpolated over, filter these out
     //NaN values are represented by line breaks
-    var data = model.data.map(function (d) {
-        return {
-            key: d[model.x.series.key],
-            value: d[series.key]
+    var data = model.data.map(function (d){
+        return{
+            key:d[model.x.series.key],
+            value: d.values[0][series.key]
         };
     }).filter(function (d) {
         return (d.y !== null);
@@ -704,41 +705,43 @@ function formatData(model, series) {
     return data;
 }
 
-function columnChart(g) {
-    'use strict';
+function columnChart(g){
+	'use strict';
 
-    var model = new DataModel(Object.create(g.data()[0]));
-    var svg = g.append('svg')
-        .attr({
-            'class': 'graphic line-chart',
-            height: model.height,
-            width: model.width,
-            xmlns: "http://www.w3.org/2000/svg",
-            version: "1.2"
-        });
-    metadata.create(svg, model);
+	var model = new DataModel(Object.create(g.data()[0]));
+	var i = model.y.series.length;
+	var svg = g.append('svg')
+		.attr({
+			'class': 'graphic line-chart',
+			height: model.height,
+			width: model.width,
+			xmlns: "http://www.w3.org/2000/svg",
+			version: "1.2"
+		});
+	metadata.create(svg, model);
 
-    var dressing = new Dressing(svg, model);
-    dressing.addHeader();
-    dressing.addFooter();
+	var dressing = new Dressing(svg, model);
+		dressing.addHeader();
+		dressing.addFooter();
 
-    var chartSVG = svg.append('g').attr('class', 'chart');
-    chartSVG.attr('transform', model.translate(model.chartPosition));
+	var chartSVG = svg.append('g').attr('class', 'chart');
+		chartSVG.attr('transform', model.translate(model.chartPosition));
 
-    var axes = new Axes(chartSVG, model);
-    axes.addValueScale();
-    if (model.groupDates) {
-        axes.addGroupedTimeScale(model.groupDates);
-    } else {
-        axes.addTimeScale();
-    }
-    axes.repositionAxis();
+	var axes = new Axes(chartSVG, model);
+		axes.addValueScale();
 
-    var plotSVG = chartSVG.append('g').attr('class', 'plot');
-    var i = model.y.series.length;
-    while (i--) {
-        plotSeries(plotSVG, model, axes, model.y.series[i]);
-    }
+	if(model.groupDates){
+		axes.addGroupedTimeScale(model.groupDates);
+	}else{
+		axes.addTimeScale();
+	}
+	axes.repositionAxis();
+
+	var plotSVG = chartSVG.append('g').attr('class', 'plot');
+
+	while(i--){
+		plotSeries(plotSVG, model, axes, model.y.series[i], i);
+	}
 }
 
 module.exports = columnChart;
@@ -819,6 +822,7 @@ function lineChart(g) {
 
     var plotSVG = chartSVG.append('g').attr('class', 'plot');
     var i = model.y.series.length;
+
     while (i--) {
         plotSeries(plotSVG, model, axes, model.y.series[i]);
     }
@@ -1427,30 +1431,30 @@ function chartWidth(model) {
     return model.contentWidth - rightGutter;
 }
 
-function setExtents(model) {
-    var extents = [];
-    model.y.series.forEach(function (l) {
-        var key = l.key;
-        model.data = model.data.map(function (d, j) {
-            var value = d[key];
-            var isValidNumber = value === null || typeof value === 'number';
-            if (!isValidNumber) {
-                model.error({
-                    node: null,
-                    message: 'Value is not a number',
-                    value: value,
-                    row: j,
-                    column: key
-                });
-            }
-            return d;
-        });
-        var ext = d3.extent(model.data, function (d) {
-            return d[key];
-        });
-        extents = extents.concat(ext);
-    });
-    return extents;
+function setExtents(model){
+	var extents = [];
+	model.y.series.forEach(function (l) {
+		var key = l.key;
+		model.data = model.data.map(function (d, j) {
+			var value = (model.groupDates) ? d.values[0][key] : d[key];
+			var isValidNumber = value === null || typeof value === 'number';
+			if (!isValidNumber) {
+				model.error({
+					node: null,
+					message: 'Value is not a number',
+					value: value,
+					row: j,
+					column: key
+				});
+			}
+			return d;
+		});
+		var ext = d3.extent(model.data, function(d){
+			return (model.groupDates) ? d.values[0][key] : d[key];
+		});
+		extents = extents.concat (ext);
+	});
+	return extents;
 }
 
 function groupedTimeDomain(model) {
@@ -1533,25 +1537,17 @@ function setKey(model) {
     return key;
 }
 
-function groupDates(m, units) {
-    var i = 0;
-    var firstDate;
-    m.data = d3.nest()
-        .key(function (d) {
-            firstDate = firstDate || d.date;
-            return dateUtil.formatter[units[0]](d.date, i++, firstDate);
-        })
-        .rollup(function (d) {
-            return d3.mean(d, function (d) {
-                return d.value;
-            });
-        })
-        .entries(m.data);
-    m.x.series.key = 'key';
-    m.y.series.forEach(function (s) {
-        s.key = 'values';
-    });
-    return m.data;
+function groupDates(m, units){
+	var i=0;
+	var firstDate;
+	m.data = d3.nest()
+		.key(function(d)  {
+			firstDate = firstDate || d.date;
+			return dateUtil.formatter[units[0]](d.date, i++, firstDate);
+		})
+		.entries(m.data);
+	m.x.series.key = 'key';
+	return m.data;
 }
 
 function Model(opts) {
@@ -1559,6 +1555,7 @@ function Model(opts) {
     var m = {
         //layout stuff
         height: undefined,
+        tickSize: 5,
         width: 300,
         chartHeight: undefined,
         chartWidth: undefined,
@@ -1599,20 +1596,22 @@ function Model(opts) {
             return d;
         });
 
-    m.contentWidth = m.width - (m.margin * 2);
-    m.chartWidth = chartWidth(m);
-    m.chartHeight = chartHeight(m);
-    m.translate = translate(0);
-    m.data = verifyData(m);
-    if (m.groupDates) {
-        m.data = groupDates(m, m.groupDates);
-        m.timeDomain = groupedTimeDomain(m);
-    } else {
-        m.timeDomain = timeDomain(m);
-    }
-    m.valueDomain = valueDomain(m);
-    m.lineStrokeWidth = lineThickness(m.lineThickness);
-    m.key = setKey(m);
+	m.contentWidth = m.width - (m.margin * 2);
+	m.chartWidth = chartWidth(m);
+	m.chartHeight = chartHeight(m);
+	m.translate = translate(0);
+	m.data = verifyData(m);
+
+	if(m.groupDates){
+		m.data = groupDates(m, m.groupDates);
+		m.timeDomain = groupedTimeDomain(m);
+	}else{
+		m.timeDomain = timeDomain(m);
+	}
+
+	m.valueDomain = valueDomain(m);
+	m.lineStrokeWidth = lineThickness(m.lineThickness);
+	m.key = setKey(m);
 
     return m;
 }
@@ -2286,11 +2285,6 @@ module.exports = "0.0.8";
 var oCharts = require('../../src/scripts/o-charts');
 var d3 = require('d3');
 
-var y = [
-    { series: ['value']},
-    { series: [{key:'value', label:'String Value'}]},
-    { series: [{key:'value', label:function(){ return 'Function Value';}}]}
-];
 var hideSource = [true, true, false];
 var numberAxisOrient = ['left', 'right', 'left'];
 
@@ -2393,6 +2387,20 @@ var fixtures = {
         { date: new Date('6/30/05'), value: 1.027},
         { date: new Date('9/30/05'), value: 1.03},
         { date: new Date('12/30/05'), value:     1.348}
+    ],
+    multiple:[
+        {
+            date: new Date('3/31/05'), value: Math.floor(Math.random() * 40) + 10, value2: 99, value3: 26
+        },
+        {
+            date: new Date('6/30/05'), value: Math.floor(Math.random() * 40) + 10, value2: 10, value3: 21
+        },
+        {
+            date: new Date('9/30/05'), value: Math.floor(Math.random() * 40) + 10, value2: 70, value3: 13
+        },
+        {
+            date: new Date('12/30/05'), value: Math.floor(Math.random() * 40) + 10, value2: 10, value3: 29
+        }
     ]
 };
 
@@ -2400,21 +2408,23 @@ var units = {
     month: ['monthly', 'yearly']
 }
 var widths = [600, 300];
-
+var series = {
+    multiple: ['value', 'value2', 'value3']
+}
 function getChartData(timeFrame){
+    var ySeries = series[timeFrame] || ['value'];
     return {
-        comment: "Column chart",
-        footnote: "this is just for testing!",
-        source: "tbc",
-        title: "Columns: " + timeFrame,
-        subtitle: "Drawn for you",
+        comment: 'Column chart',
+        footnote: 'this is just for testing!',
+        source: 'tbc',
+        title: 'Columns: ' + timeFrame,
+        subtitle: 'Drawn for you',
         numberAxisOrient: 'left', //todo: refactor onto y object
         hideSource: false,
         x:{
           series: {key:'date', label:'yearly'}
         },
-        tickSize: 5,
-        y: { series: ['value'] },
+        y: { series: ySeries} ,
         groupDates: units[timeFrame] || ['quarterly', 'yearly'],
         data: fixtures[timeFrame]
     };
@@ -2423,17 +2433,18 @@ function getChartData(timeFrame){
 module.exports = {
     getChartData: getChartData,
     init: function(){
-        var demos = ['year','yearWithNegative','years','yearsWithNegative','decade', 'month'];
+        var demos = ['year','yearWithNegative','years','yearsWithNegative','decade', 'month', 'multiple'];
+        //var demos = ['multiple'];
         demos.forEach(function(timeFrame){
             d3.select('#views').append('div').attr({
                 'id':'column-chart__' + timeFrame
             });
-            widths.forEach(function(width){
+            widths.forEach(function (width){
                 var data = getChartData(timeFrame);
                 data.width = width;
-                d3.select('#column-chart__' + timeFrame).append('span').attr({
-                    'class':'width' + width
-                }).data([data]).call( oCharts.chart.column );
+                d3.select('#column-chart__' + timeFrame).append('span')
+                    .attr('class', 'width' + width)
+                    .data([data]).call(oCharts.chart.column);
             });
         });
     }
--- scripts/o-charts.js
+++ scripts/o-charts.js
@@ -664,39 +664,40 @@ var DataModel = require('../util/data.model.js');
 var metadata = require('../util/metadata.js');
 var Dressing = require('../util/dressing.js');
 
-function plotSeries(plotSVG, model, axes, series) {
+function plotSeries(plotSVG, model, axes, series, i){
+	var data = formatData(model, series);
+	var s = plotSVG.append('g')
+		.attr('class', 'series');
 
-    var data = formatData(model, series);
+//todo:  sort out scale so you dont have to do the maths for width & x-position
+//	http://bl.ocks.org/mbostock/3887051
+//console.log(data)
+//	var x1= d3.scale.ordinal()
+//	var bars = d3.keys(data).filter(function(key) { return key !== "State"; });
+//	console.log(bars)
+//	x1.domain(bars).rangeRoundBands([0, axes.timeScale.rangeBand()]);
 
-    plotSVG.selectAll("rect")
+    s.selectAll('rect')
         .data(data)
         .enter()
-        .append("rect")
-        .attr("class", function (d) {
-            return "column column--" + series.className + (d.value < 0 ? " negative" : " positive");
-        })
-        .attr("data-value", function (d) {
-            return d.value;
-        })
-        .attr("x", function (d) {
-            return axes.timeScale(d.key);
-        })
-        .attr("y", function (d) {
-            return axes.valueScale(Math.max(0, d.value));
-        })
-        .attr("height", function (d) {
-            return Math.abs(axes.valueScale(d.value) - axes.valueScale(0));
-        })
-        .attr("width", axes.timeScale.rangeBand());
+        .append('rect')
+        .attr('class', function (d){return 'column column--'  + series.className + (d.value < 0 ? ' negative' : ' positive');})
+        .attr('data-value', function (d){	return d.value;})
+		.attr('x', function (d){return axes.timeScale(d.key) + ((axes.timeScale.rangeBand() / model.y.series.length) * i);}) //adjust the x position based on the series number
+		//.attr("x", function(d) { return x1(d.key); })
+		.attr('y', function (d){return axes.valueScale(Math.max(0, d.value));})
+        .attr('height', function (d){return 	Math.abs(axes.valueScale(d.value) - axes.valueScale(0));})
+		//.attr("width", x1.rangeBand())
+        .attr('width', axes.timeScale.rangeBand() / model.y.series.length); //width is divided by series length
 }
 
 function formatData(model, series) {
     //null values in the data are interpolated over, filter these out
     //NaN values are represented by line breaks
-    var data = model.data.map(function (d) {
-        return {
-            key: d[model.x.series.key],
-            value: d[series.key]
+    var data = model.data.map(function (d){
+        return{
+            key:d[model.x.series.key],
+            value: d.values[0][series.key]
         };
     }).filter(function (d) {
         return (d.y !== null);
@@ -704,41 +705,43 @@ function formatData(model, series) {
     return data;
 }
 
-function columnChart(g) {
-    'use strict';
+function columnChart(g){
+	'use strict';
 
-    var model = new DataModel(Object.create(g.data()[0]));
-    var svg = g.append('svg')
-        .attr({
-            'class': 'graphic line-chart',
-            height: model.height,
-            width: model.width,
-            xmlns: "http://www.w3.org/2000/svg",
-            version: "1.2"
-        });
-    metadata.create(svg, model);
+	var model = new DataModel(Object.create(g.data()[0]));
+	var i = model.y.series.length;
+	var svg = g.append('svg')
+		.attr({
+			'class': 'graphic line-chart',
+			height: model.height,
+			width: model.width,
+			xmlns: "http://www.w3.org/2000/svg",
+			version: "1.2"
+		});
+	metadata.create(svg, model);
 
-    var dressing = new Dressing(svg, model);
-    dressing.addHeader();
-    dressing.addFooter();
+	var dressing = new Dressing(svg, model);
+		dressing.addHeader();
+		dressing.addFooter();
 
-    var chartSVG = svg.append('g').attr('class', 'chart');
-    chartSVG.attr('transform', model.translate(model.chartPosition));
+	var chartSVG = svg.append('g').attr('class', 'chart');
+		chartSVG.attr('transform', model.translate(model.chartPosition));
 
-    var axes = new Axes(chartSVG, model);
-    axes.addValueScale();
-    if (model.groupDates) {
-        axes.addGroupedTimeScale(model.groupDates);
-    } else {
-        axes.addTimeScale();
-    }
-    axes.repositionAxis();
+	var axes = new Axes(chartSVG, model);
+		axes.addValueScale();
 
-    var plotSVG = chartSVG.append('g').attr('class', 'plot');
-    var i = model.y.series.length;
-    while (i--) {
-        plotSeries(plotSVG, model, axes, model.y.series[i]);
-    }
+	if(model.groupDates){
+		axes.addGroupedTimeScale(model.groupDates);
+	}else{
+		axes.addTimeScale();
+	}
+	axes.repositionAxis();
+
+	var plotSVG = chartSVG.append('g').attr('class', 'plot');
+
+	while(i--){
+		plotSeries(plotSVG, model, axes, model.y.series[i], i);
+	}
 }
 
 module.exports = columnChart;
@@ -819,6 +822,7 @@ function lineChart(g) {
 
     var plotSVG = chartSVG.append('g').attr('class', 'plot');
     var i = model.y.series.length;
+
     while (i--) {
         plotSeries(plotSVG, model, axes, model.y.series[i]);
     }
@@ -1408,30 +1412,30 @@ function chartWidth(model) {
     return model.contentWidth - rightGutter;
 }
 
-function setExtents(model) {
-    var extents = [];
-    model.y.series.forEach(function (l) {
-        var key = l.key;
-        model.data = model.data.map(function (d, j) {
-            var value = d[key];
-            var isValidNumber = value === null || typeof value === 'number';
-            if (!isValidNumber) {
-                model.error({
-                    node: null,
-                    message: 'Value is not a number',
-                    value: value,
-                    row: j,
-                    column: key
-                });
-            }
-            return d;
-        });
-        var ext = d3.extent(model.data, function (d) {
-            return d[key];
-        });
-        extents = extents.concat(ext);
-    });
-    return extents;
+function setExtents(model){
+	var extents = [];
+	model.y.series.forEach(function (l) {
+		var key = l.key;
+		model.data = model.data.map(function (d, j) {
+			var value = (model.groupDates) ? d.values[0][key] : d[key];
+			var isValidNumber = value === null || typeof value === 'number';
+			if (!isValidNumber) {
+				model.error({
+					node: null,
+					message: 'Value is not a number',
+					value: value,
+					row: j,
+					column: key
+				});
+			}
+			return d;
+		});
+		var ext = d3.extent(model.data, function(d){
+			return (model.groupDates) ? d.values[0][key] : d[key];
+		});
+		extents = extents.concat (ext);
+	});
+	return extents;
 }
 
 function groupedTimeDomain(model) {
@@ -1514,25 +1518,17 @@ function setKey(model) {
     return key;
 }
 
-function groupDates(m, units) {
-    var i = 0;
-    var firstDate;
-    m.data = d3.nest()
-        .key(function (d) {
-            firstDate = firstDate || d.date;
-            return dateUtil.formatter[units[0]](d.date, i++, firstDate);
-        })
-        .rollup(function (d) {
-            return d3.mean(d, function (d) {
-                return d.value;
-            });
-        })
-        .entries(m.data);
-    m.x.series.key = 'key';
-    m.y.series.forEach(function (s) {
-        s.key = 'values';
-    });
-    return m.data;
+function groupDates(m, units){
+	var i=0;
+	var firstDate;
+	m.data = d3.nest()
+		.key(function(d)  {
+			firstDate = firstDate || d.date;
+			return dateUtil.formatter[units[0]](d.date, i++, firstDate);
+		})
+		.entries(m.data);
+	m.x.series.key = 'key';
+	return m.data;
 }
 
 function Model(opts) {
@@ -1540,6 +1536,7 @@ function Model(opts) {
     var m = {
         //layout stuff
         height: undefined,
+        tickSize: 5,
         width: 300,
         chartHeight: undefined,
         chartWidth: undefined,
@@ -1580,20 +1577,22 @@ function Model(opts) {
             return d;
         });
 
-    m.contentWidth = m.width - (m.margin * 2);
-    m.chartWidth = chartWidth(m);
-    m.chartHeight = chartHeight(m);
-    m.translate = translate(0);
-    m.data = verifyData(m);
-    if (m.groupDates) {
-        m.data = groupDates(m, m.groupDates);
-        m.timeDomain = groupedTimeDomain(m);
-    } else {
-        m.timeDomain = timeDomain(m);
-    }
-    m.valueDomain = valueDomain(m);
-    m.lineStrokeWidth = lineThickness(m.lineThickness);
-    m.key = setKey(m);
+	m.contentWidth = m.width - (m.margin * 2);
+	m.chartWidth = chartWidth(m);
+	m.chartHeight = chartHeight(m);
+	m.translate = translate(0);
+	m.data = verifyData(m);
+
+	if(m.groupDates){
+		m.data = groupDates(m, m.groupDates);
+		m.timeDomain = groupedTimeDomain(m);
+	}else{
+		m.timeDomain = timeDomain(m);
+	}
+
+	m.valueDomain = valueDomain(m);
+	m.lineStrokeWidth = lineThickness(m.lineThickness);
+	m.key = setKey(m);
 
     return m;
 }
