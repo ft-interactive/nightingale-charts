321c3e70bc6fd91d0ebe7c05825075788f18b71d
Peter Mouland <peter.mouland@gmail.com>
2015-05-05 13:56:21 +0100

    tidy up demo code

--- demo/scripts/line-chart.js
+++ demo/scripts/line-chart.js
@@ -1,23 +1,13 @@
 var oCharts = require('../../src/scripts/o-charts');
 var d3 = require('d3');
 
-var y = [
-            {
-                series: ['value', 'value2', 'value3']
+var y = [   { series: ['value', 'value2', 'value3'] },
+            { series: [  {key:'value', label:'String Value'},
+                         {key:'value2', label:'Another String Value'} ]
             },
-            {
-                series: [
-                            {key:'value', label:'String Value'},
-                            {key:'value2', label:'Another String Value'}
-                        ]
-            },
-            {
-                series: [
-                            {key:'value', label:function(){ return 'Function Value';}},
-                            {key:'value2', label:function(){ return 'Another function Value';}}
-                        ]
-            }
-        ];
+            { series: [ {key:'value', label:function(){ return 'Function Value';}},
+                        {key:'value2', label:function(){ return 'Another function Value';}} ]
+            } ];
 var hideSource = [true, true, false];
 var numberAxisOrient = ['left', 'right', 'left'];
 
@@ -35,7 +25,7 @@ function getChartData(i) {
         },
         y: y[i],
         data: [
-            {date: new Date('2000-01-01T00:00:00.000Z'), value: Math.random() * 40, value2: 150, value3:66},
+            {date: new Date('2000-01-01T00:00:00.000Z'), value: Math.random() * 40, value2: Math.random() * 40, value3:66},
             {date: new Date('2001-01-01T00:00:00.000Z'), value: Math.random() * 40, value2: Math.random() * 40, value3:66},
             {date: new Date('2002-01-01T00:00:00.000Z'), value: Math.random() * 40, value2: Math.random() * 40, value3:66},
             {date: new Date('2003-01-01T00:00:00.000Z'), value: Math.random() * 40, value2: Math.random() * 40, value3:66}
