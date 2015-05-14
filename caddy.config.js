2751350bb22a0e0731a931d71eaea07d0e4a8920
Peter Mouland <peter.mouland@gmail.com>
2015-05-14 10:30:23 +0100

    move to using caddy build tool

---
+++ caddy.config.js
@@ -0,0 +1,32 @@
+var pkg = require('./package.json');
+
+module.exports = {
+    pkg: pkg,
+    paths: {
+        source: "./src",
+        "demo": "./demo",
+        "target": './_site'
+    },
+    tasks: {
+        build: ['sass', 'mustache', 'browserify'],
+        serve: 'staticApp',
+        test: 'karma',
+        release: ['git', 'gh-pages']
+    },
+    browserify: {
+        insertGlobals: false,
+        detectGlobals: false,
+        noParse: [
+            './bower_components/d3/d3.js'
+        ],
+        vendorBundle: [
+            {file: './bower_components/d3/d3.js', expose: 'd3'}
+        ],
+        vendorTarget: 'scripts/vendor.js'
+    },
+    karma:{
+        functional: './test/karma.functional.js', // string or false. Karma config file.
+        unit: './test/karma.unit.js', // string or false. Karma config file with coverage setup.
+        unitCoverage: './test/coverage/summary.json'// code coverage summary for unit tests
+    }
+};
