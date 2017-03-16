08f6bbbddd1422b7a996b0507f7b57746e70153c
jonathan-best <jonathan.best@ft.com>
2017-03-16 17:05:17 +0000

    tweak

--- src/scripts/util/data.model.js
+++ src/scripts/util/data.model.js
@@ -25,7 +25,7 @@ function chartWidth(model) {
     var rightGutter = model.contentWidth < 260 ? 16 : 26;
     var fullWidthChart = themes.check(model.theme, 'chart-plot').attributes['full-width'] || false;
     if (fullWidthChart) {
-      return model.contentWidth - rightGutter;
+      return model.chartType === 'bar' ? model.contentWidth - rightGutter : model.contentWidth;
     }
 
     if (model.paddingX) rightGutter = 0;
