4dd4d0acb4f5012427be8550b972ce9b1a6cc579
jonathan-best <jonathan.best@ft.com>
2017-07-19 10:40:04 +0100

    Add secret squirrel  🐿2.5.10

---
+++ secret-squirrel.js
@@ -0,0 +1,30 @@
+module.exports = {
+	files: {
+		allow: [
+			'.jshintrc',
+			'CONTRIBUTING.MD',
+			'src/scripts/fonts/Avenir-Heavy.txt',
+			'src/scripts/fonts/Avenir-Light.txt',
+			'src/scripts/fonts/Avenir-LightOblique.txt',
+			'src/scripts/fonts/BentonSans.txt',
+			'src/scripts/fonts/MetricWeb.txt',
+			'src/scripts/fonts/MetricWebSemiBold.txt'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'z3nezoupMcbUm+fNt8zmI8MvjprIP597403TO/lP', // src/scripts/fonts/BentonSans.txt:1
+			'9hsHjdNpt3etWXNR0H5lZ', // src/scripts/fonts/BentonSans.txt:1
+			'4ueOVCsN39bzJini4PmIDgMhvl9FMTAC', // src/scripts/fonts/BentonSans.txt:1
+			'R11BT5L0RFYKZEVgykZx6YlHT8SvNmlckswoWLcK', // src/scripts/fonts/BentonSans.txt:1
+			'CpWelsQDLJfr8GUyz9NmHbQZvsgKzpYoMybcZ0hIyWYDRitlltnCnkZqaQdDiDVr0adGnxRu06NOiT4s', // src/scripts/fonts/BentonSans.txt:1
+			'0wDSwN9AAAACQACAAAAAAAAeNpjYGRgYK797wMki', // src/styles/_metric.scss:4|4
+			'CUZSKBu0TcuIDUdV3uurf', // src/styles/_metric.scss:4
+			'wdbwy4H1yuwZkq4xsR7FpwGH4HfwafgB', // src/styles/_metric.scss:4
+			'sCCHhlcQ2JXHpFH6QTaA7AzuYji4sdtlnxzWoywArAl5VnQWF5QMjnP8SiHZmHA3zFH60AZAGQrLEJxB', // src/styles/_metric.scss:4
+			'value4value4value4value4' // test/fixtures/scripts/column-chart.js:272|273|274|275|276|368
+		]
+	}
+};
